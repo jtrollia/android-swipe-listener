8460ad07975dea87bc9d472a1731cd1c1a4d480c
Malivuk <jl.trolliard@gmail.com>
2015-06-20 14:09:37 +0900

    2d commit

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # android-swipe-listener
 Listen for swipe gestures.
+
+// Override method in an Activity
+RelativeLayout myLayout = (RelativeLayout) v.findViewById(R.id.{LAYOUT});
+myLayout.setOnTouchListener(new SwipeListener(getActivity()) {
+
+    @Override
+    public void onSwipeLeft() {
+
+        // ...
+    }
+});
+
+}
---
+++ SwipeListener.java
@@ -0,0 +1,60 @@
+package com.{package};
+
+
+import android.content.Context;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.view.View;
+
+
+public class SwipeListener implements View.OnTouchListener {
+
+    public Context context;
+    public GestureDetector gestureDetector;
+
+    public SwipeListener(Context ctx) {
+        context = ctx;
+        gestureDetector = new GestureDetector(ctx, new GestureListener());
+    }
+
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        return gestureDetector.onTouchEvent(event);
+    }
+
+    // Interface
+    public void onSwipeLeft() {
+    }
+
+    private final class GestureListener extends GestureDetector.SimpleOnGestureListener {
+
+        @Override
+        public boolean onDown(MotionEvent e) {
+            return true;
+        }
+
+        @Override
+        public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+            final int SWIPE_MIN_DISTANCE = 120;
+            final int SWIPE_MAX_OFF_PATH = 250;
+            final int SWIPE_THRESHOLD_VELOCITY = 200;
+
+            try {
+                if (Math.abs(e1.getY() - e2.getY()) > SWIPE_MAX_OFF_PATH)
+                    return false;
+                if (e1.getX() - e2.getX() > SWIPE_MIN_DISTANCE && Math.abs(velocityX) > SWIPE_THRESHOLD_VELOCITY) {
+
+                    // Close fragment
+                    onSwipeLeft();
+
+                } else if (e2.getX() - e1.getX() > SWIPE_MIN_DISTANCE && Math.abs(velocityX) > SWIPE_THRESHOLD_VELOCITY) {
+                    return false;
+                }
+            } catch (Exception e) {
+                // nothing
+            }
+
+            return super.onFling(e1, e2, velocityX, velocityY);
+        }
+    }
+}
